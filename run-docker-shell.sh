3af2df8c3f4a59c600c0ac21def477152dc10687
Paul Ferraro <paul.ferraro@redhat.com>
2023-08-24 09:45:51 -0400

    Create wfcore user within Dockerfile using same uid as host user, replacing need for an entrypoint. Remove the need for problematic uid/gid mapping, which otherwise messes with ownership of directories on the host filesystem that are exposed to the container.

--- run-docker-shell.sh
+++ run-docker-shell.sh
@@ -2,11 +2,13 @@
 
 docker run \
   --memory=4G \
+  --userns keep-id \
+  --group-add keep-groups \
 	-v ~/.m2/settings.xml:/home/wfcore/.m2/settings.xml \
 	-v wfcore-release-maven-repo:/home/wfcore/.m2 \
 	-v wfcore-release-checkouts:/home/wfcore/checkouts \
 	-v ~/.gitconfig:/home/wfcore/.gitconfig \
 	-v ~/.ssh:/home/wfcore/.ssh \
-    -v ~/.gnupg:/home/wfcore/.gnupg \
+ 	-v ~/.gnupg:/home/wfcore/.gnupg \
 	-it ${USER}/wildfly-core-release:11 \
   bash
